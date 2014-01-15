7a08f71b28834e8540b2f4d4f175d38b3a508cb1
Jun Rekimoto <rekimoto@acm.org>
2014-01-15 10:27:58 +0900

    C

--- vocab.rb
+++ vocab.rb
@@ -2,14 +2,14 @@
 
 # (c)2014  Jun Rekimoto
 # compares your vocabulary with reference documents, 
-# suggest words that you don't use in your documents.
+# suggests words that you don't use in your documents.
 # document types are either .txt, .tex, .pdf, .doc, and .docx
-# usage:  ruby vocab.rb YOUR-DOCUMENT-DIRECTORY REFERENCE-DOCUMENT-DIRECTORY
 
-# prerequisites
-# TreeTagger 
-# pdftotxt (contained in xpdf package)
+# Usage:  ruby vocab.rb YOUR-DOCUMENT-DIRECTORY REFERENCE-DOCUMENT-DIRECTORY
 
+# Prerequisites
+# TreeTagger  http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/
+# pdftotxt ( brew install xpdf )
 
 $lex = "tree-tagger/cmd/tree-tagger-english"  # English lexical analysis command (tree-tagger)
 
@@ -21,11 +21,9 @@ def mkhist(fname, hist)
 		text = `cat \"#{fname}\" | #{$lex}`
 	elsif fname =~ /\.pdf$/
 		text = `pdftotext -q \"#{fname}\" - | #{$lex}`
-		print "Text size #{text.length}\n"
 	elsif fname =~ /\.doc$/ || fname =~ /\.docx$/
 		text = `textutil -stdout -convert txt \"#{fname}\" | #{$lex}`
 	end
-
 	return if text == nil
 
 	wcount = 0
@@ -38,7 +36,7 @@ def mkhist(fname, hist)
 				part = "Ad" if tkn[1] =~ /^JJ/ # adjective
 				part = "Adv" if tkn[1] =~ /^RB/ # adverb
 				part = "N" if tkn[1] =~ /^NN/ # noun
-
+				part = "C" if tkn[1] =~ /^CC/ || tkn[1] =~ /^IN/ # conjunction/preposition
 				if part
 					word = tkn[2]   # verb base form
 					hist[part][word] = 0 if hist[part][word] == nil
@@ -51,76 +49,63 @@ def mkhist(fname, hist)
 	}
 
 	$stderr.print "#{wcount} word(s) are extracted\n\n"
+	return wcount
 end
 
+$part = ["V", "Ad", "Adv", "N", "C"]
 
-$suffix = "tex,txt,doc,docx,pdf"
-$yourdir = nil
-$refdir = nil
-$numsuggest = 100
+# scan directories recursively and create a word histgram
+def scandir(dir)
+	filecount = 0
+	wordcount = 0
+	hist = {}
+	$part.each{|p| hist[p] = {}}
+	Dir::glob("#{dir}/**/*.{tex,txt,doc,docx,pdf}").each {|fname|
+		$stderr.print ">>>> #{fname}\n"
+		begin
+ 			wordcount += mkhist(fname, hist)
+ 			filecount += 1
+ 		rescue
+ 		end
+ 	}
+ 	return hist, filecount, wordcount
+end
+
+# save histgram to files
+def savehist(hist, name) 
+	hist_sort = {}
+	hist.each {|part, h|
+		hist_sort[part] = h.to_a.sort{ |a, b| b[1]<=>a[1]}
+		File.open("#{name}#{part}.txt", "w") {|f| 
+			hist_sort[part].each {|word, occurrence|
+				f.print "#{word} #{occurrence}\n"
+			}
+		}
+	}
+	return hist_sort
+end
+
+yourdir = nil
+refdir = nil
+numsuggest = 100
 
 if ARGV.length == 2
-	$yourdir = ARGV[0]
-	$refdir = ARGV[1]
+	yourdir = ARGV[0]
+	refdir = ARGV[1]
 else
 	print "Usage: ruby vocab.rb YOUR_DIRECTORY REFERENCE_DIRECTORY\n"
 	exit
 end
 
-$yourhist = {}
-$yourhist["V"] = {}
-$yourhist["Ad"] = {}
-$yourhist["Adv"] = {}
-$yourhist["N"] = {}
-yourcount = 0
-Dir::glob("#{$yourdir}/**/*.{#{$suffix}}").each {|fname|
-	$stderr.print "***** #{fname}\n"
-	begin
- 		mkhist(fname, $yourhist)
- 		yourcount += 1
- 	rescue
- 	end
-}
+yourhist, yourcount, yourwords = scandir(yourdir)
+yourhist_sort = savehist(yourhist, "your")
 
+refhist, refcount, refwords = scandir(refdir)
+refhist_sort = savehist(refhist, "ref")
 
-$yourhist_sort = {}
-$yourhist.each {|part, hist|
-	$yourhist_sort[part] = hist.to_a.sort{ |a, b| b[1]<=>a[1]}
-	File.open("your#{part}.txt", "w") {|f| 
-		$yourhist_sort[part].each {|word, occurrence|
-			f.print "#{word} #{occurrence}\n"
-		}
-	}
-}
-
-
-$refhist = {}
-$refhist["V"] = {}
-$refhist["Ad"] = {}
-$refhist["Adv"] = {}
-$refhist["N"] = {}
-refcount = 0
-Dir::glob("#{$refdir}/**/*.{#{$suffix}}").each {|fname|
-	$stderr.print ">>>>> #{fname}\n"
-	begin
- 		mkhist(fname, $refhist)
- 		refcount += 1
- 	rescue
- 	end
-}
-$refhist_sort = {}
-$refhist.each {|part, hist|
-	$refhist_sort[part] = hist.to_a.sort{ |a, b| b[1]<=>a[1]}
-	File.open("ref#{part}.txt", "w") {|f| 
-		$refhist_sort[part].each {|word, occurrence|
-			f.print "#{word} #{occurrence}\n"
-		}
-	}
-}
-
-
+# Create a suggestions file containing words that you don't use in your documents
 File.open("suggestions.txt", "w") { |f|
-	$refhist_sort.each {|part, hist|
+	refhist_sort.each {|part, hist|
 		index = 0
 		n = 0
 		f.print "*** suggestions for #{part}\n"
@@ -128,29 +113,26 @@ File.open("suggestions.txt", "w") { |f|
 		f.print "---------------------------\n"
 		hist.each {|word, count|
 			index += 1
-			if $yourhist[part][word] == nil
+			if yourhist[part][word] == nil
 				f.print "#{word} #{index} #{count} #{part}\n"
 				n += 1
 			end
-			break if n >= $numsuggest
+			break if n >= numsuggest
 		}
 		f.print "\n"
 	}
 }
 
-
-printf "Your documents: %4d files ", yourcount
-$yourhist.each{|part, hist| printf " %s(%4d)", part, hist.length}
-print " words\n"
-printf "Ref  documents: %4d files ", refcount
-$refhist.each{|part, hist| printf " %s(%4d)", part, hist.length}
-print " words\n"
-printf "                          "
-$yourhist.each{|part, hist| printf " %s  %1.2f", " "*part.length, $refhist[part].length.to_f / hist.length}
+printf "Yours: %4d files ", yourcount
+yourhist.each{|part, hist| printf " %s(%4d)", part, hist.length}
+print "\n"
+printf "Ref  : %4d files ", refcount
+refhist.each{|part, hist| printf " %s(%4d)", part, hist.length}
+print " \n"
+printf "Ref/Yours:       "
+yourhist.each{|part, hist| printf " %s  %1.2f", " "*part.length, refhist[part].length.to_f / hist.length}
 print "\n\n"
 
-print "your{V,Ad,Adv,N}.txt: your word histgram\n"
-print "ref{V,Ad,Adv}.txt: reference word histgram\n"
+print "your{#{$part.join(',')}}.txt: your word histgram\n"
+print "ref{#{$part.join(',')}}.txt: reference word histgram\n"
 print "suggestions.txt: suggestions\n"
-
-
